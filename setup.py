6e40867dc2dc107698c3c6d9d8ebdcbba995b63d
Yasset Perez-Riverol <ypriverol@gmail.com>
2024-05-24 10:53:20 +0100

    add sage features

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ CLASSIFIERS = [
     "Development Status :: 5 - Production/Stable",
 ]
 
-INSTALL_REQUIRES = ["click", "sdrf_pipelines"]
+INSTALL_REQUIRES = ["click", "sdrf_pipelines", "pyopenms"]
 PYTHON_REQUIRES = ">=3.7,<4"
 
 with open("README.md", "r") as fh:
