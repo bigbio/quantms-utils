841f28330f7aae42f771df7a67d9042e5f282ad3
Yasset Perez-Riverol <ypriverol@gmail.com>
2024-08-12 11:24:29 +0100

    added more spectrum features to quantmsutilsc.py

--- quantmsutils/features/snr.py
+++ quantmsutils/features/snr.py
@@ -4,6 +4,7 @@ import click
 import numpy as np
 from pyopenms import MzMLFile, MSExperiment, SpectrumLookup
 import pyopenms as oms
+from scipy.stats import entropy
 
 
 def compute_signal_to_noise(intensities):
@@ -20,7 +21,7 @@ def compute_signal_to_noise(intensities):
     return snr
 
 
-@click.command("snr2feature")
+@click.command("spectrum2feature")
 @click.option("--ms_path", type=click.Path(exists=True), required=True)
 @click.option(
     "--idxml",
@@ -33,7 +34,7 @@ def compute_signal_to_noise(intensities):
     required=True,
 )
 @click.pass_context
-def snr2feature(ctx, ms_path: str, idxml: str, output: str) -> None:
+def spectrum2feature(ctx, ms_path: str, idxml: str, output: str) -> None:
     """
     The snr function computes the signal-to-noise ratio for a given mass spectrometry file and its corresponding
     identification file.
@@ -64,9 +65,29 @@ def snr2feature(ctx, ms_path: str, idxml: str, output: str) -> None:
             index = lookup.findByScanNumber(scan_number)
             spectrum = mzml_file.getSpectrum(index)
             intensity_array = spectrum.get_peaks()[1]
+            mz_array = spectrum.get_peaks()[0]
+
+            ## Compute signal to noise ratio
             snr = compute_signal_to_noise(intensity_array)
+
+            # Spectral Entropy
+            tic = np.sum(intensity_array)
+            normalized_intensities = intensity_array / tic
+            spectral_entropy = entropy(normalized_intensities)
+
+            # Fraction of TIC in Top 10 Peaks
+            top_n_peaks = np.sort(intensity_array)[-10:]
+            fraction_tic_top_10 = np.sum(top_n_peaks) / tic
+
+            # Intensity Weighted m/z Standard Deviation
+            weighted_mean_mz = np.sum(np.array(mz_array) * normalized_intensities)
+            weighted_std_mz = np.sqrt(np.sum(normalized_intensities * (np.array(mz_array) - weighted_mean_mz) ** 2))
+
             for hit in peptide.getHits():
                 hit.setMetaValue("quantms:SNR", str(snr))
+                hit.setMetaValue("quantms:SpectralEntropy", str(spectral_entropy))
+                hit.setMetaValue("quantms:FracTICinTop10Peaks", str(fraction_tic_top_10))
+                hit.setMetaValue("quantms:WeightedStdMz", str(weighted_std_mz))
         except IndexError:
             message = "scan_number" + str(scan_number) + "not found in file: " + ms_path
             print(message)
@@ -74,7 +95,7 @@ def snr2feature(ctx, ms_path: str, idxml: str, output: str) -> None:
     # Add quantms:SNR as a feature
     search_parameters = protein_ids[0].getSearchParameters()
     features = search_parameters.getMetaValue("extra_features")
-    extra_features = features + ",quantms:SNR"
+    extra_features = features + ",quantms:SNR, quantms:SpectralEntropy, quantms:FracTICinTop10Peaks, quantms:WeightedStdMz"
     search_parameters.setMetaValue("extra_features", extra_features)
     protein_ids[0].setSearchParameters(search_parameters)
 
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -101,7 +101,7 @@ def test_snr_file():
     result = runner.invoke(
         cli,
         [
-            "snr2feature",
+            "spectrum2feature",
             "--ms_path",
             "tests/test_data/TMT_Erwinia_1uLSike_Top10HCD_isol2_45stepped_60min_01.mzML",
             "--idxml",
