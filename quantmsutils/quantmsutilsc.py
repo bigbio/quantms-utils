e85331c8f6f781b9399b2df60ba45a69c89566a1
Yasset Perez-Riverol <ypriverol@gmail.com>
2025-02-20 14:56:38 +0000

    REmove the feature commands -> quantms-rescoring

--- quantmsutils/quantmsutilsc.py
+++ quantmsutils/quantmsutilsc.py
@@ -2,12 +2,10 @@ import click
 
 from quantmsutils.diann.diann2mztab import diann2mztab
 from quantmsutils.diann.dianncfg import dianncfg
-from quantmsutils.features.sage_feature import add_sage_feature
 from quantmsutils.mzml.mzml_statistics import mzml_statistics
 from quantmsutils.psm.psm_conversion import convert_psm
 from quantmsutils.sdrf.check_samplesheet import checksamplesheet
 from quantmsutils.sdrf.extract_sample import extract_sample_from_expdesign
-from quantmsutils.features.snr import spectrum2feature
 from quantmsutils import __version__
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
@@ -23,12 +21,10 @@ def cli():
 
 cli.add_command(dianncfg)
 cli.add_command(diann2mztab)
-cli.add_command(add_sage_feature)
 cli.add_command(mzml_statistics)
 cli.add_command(extract_sample_from_expdesign)
 cli.add_command(checksamplesheet)
 cli.add_command(convert_psm)
-cli.add_command(spectrum2feature)
 
 
 def main():
